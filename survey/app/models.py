15f1a62ee43107ac87b4235c82200a386a9a608d
Андрей Шакиров <AnShakirov@alabuga.local>
2025-01-09 15:17:02 +0300

    сделанны модели вопросов и опрсов

--- survey/app/models.py
+++ survey/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    multiple = models.BooleanField(default=False)
+    type = models.CharField(choices=(('int', 'int'), ('string', 'string'), ("choices", "choices")), max_length=32)
+    Necessarily = models.BooleanField(default=True)
+
+
+class Survey(models.Model):
+    questions = models.ManyToManyField(Question)
--- survey/app/views.py
+++ survey/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import routers, viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({"message": "This is a protected view."})
